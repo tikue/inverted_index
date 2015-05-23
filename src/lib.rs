45a07e0aec4685b1e7e3b5f6157cd47310d71284
Tim Kuehn <tim@solvvy.com>
2015-05-22 22:58:45 -0700

    Remove unsafe; make Document fields private; use getters.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
+#![feature(collections)]
 extern crate shared_slice;
 
 use std::collections::{BTreeMap, HashSet};
 use std::hash::{Hash, Hasher};
-use std::str::from_utf8;
+use std::str::from_utf8_unchecked;
 use shared_slice::rc::RcStr;
  
 /// A Document contains an id and content, both of which are reference-counted strings.
 /// Hashing and equality are based only on the id field.
 #[derive(Clone, Eq)]
 pub struct Document {
-    pub id: RcStr,
-    pub content: RcStr,
+    id: RcStr,
+    content: RcStr,
 }
 
 impl Document {
@@ -24,6 +25,14 @@ impl Document {
             id: into_rc_str(id),
             content: into_rc_str(content),
         }
+    }
+
+    pub fn id(&self) -> &str {
+        unsafe { from_utf8_unchecked(&*self.id) }
+    }
+
+    pub fn content(&self) -> &str {
+        unsafe { from_utf8_unchecked(&*self.content) }
     }
 }
  
@@ -47,8 +56,7 @@ impl PartialEq<Document> for Document {
  
 impl std::fmt::Debug for Document {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(fmt, "Document {{ id: {:?}, content: {:?} }}", 
-               from_utf8(&*self.id).unwrap(), from_utf8(&*self.content).unwrap())
+        write!(fmt, "Document {{ id: {:?}, content: {:?} }}", self.id(), self.content())
     }
 }
 
@@ -71,8 +79,13 @@ impl Index for InvertedIndex {
     /// A basic implementation of index, splits the document's content into whitespace-separated
     /// words, and inserts each word-document pair into the map.
     fn index(&mut self, doc: Document) {
-        for word in doc.content.clone().split_whitespace() {
-            self.entry(word).or_insert_with(|| HashSet::new()).insert(doc.clone());
+        let lowercased = doc.content().to_lowercase();
+        let ngrams = into_rc_str(lowercased)
+            .split_whitespace()
+            .flat_map(|word: RcStr| (1..word.len() + 1).map(move |to| word.clone().slice_to(to)));
+
+        for ngram in ngrams {
+            self.entry(ngram).or_insert_with(|| HashSet::new()).insert(doc.clone());
         }
     }
  
@@ -88,17 +101,34 @@ impl Index for InvertedIndex {
 }
 
 #[test]
-fn test_index() {
-    let mut index = BTreeMap::new();    
-
+fn test_search1() {
+    let mut index = InvertedIndex::new();    
     let doc1 = Document::new("1", "learn to program in rust today");
-    index.index(doc1.clone());
-
     let doc2 = Document::new("2", "what did you do today");
+    index.index(doc1.clone());
     index.index(doc2.clone());
-
     let search_results = index.search("today");
     assert!(search_results.contains(&doc1));
     assert!(search_results.contains(&doc2));
-    assert_eq!(2, index.search("rust today").len())
+}
+
+#[test]
+fn test_ngrams() {
+    let mut index = InvertedIndex::new();    
+    let doc1 = Document::new("1", "learn to program in rust today");
+    let doc2 = Document::new("2", "what did you do today");
+    index.index(doc1.clone());
+    index.index(doc2.clone());
+    let search_results = index.search("to");
+    assert!(search_results.contains(&doc1));
+    assert!(search_results.contains(&doc2));
+}
+
+#[test]
+fn test_search2() {
+    let mut index = InvertedIndex::new();    
+    index.index(Document::new("2", "what did you do today"));
+    index.index(Document::new("3", "hey today"));
+    let search_results = index.search("today");
+    assert_eq!(search_results.len(), 2);
 }
