ebbeda587103d3ddaa275ae8a3933f8a207ec504
Tim Kuehn <timothy.j.kuehn@gmail.com>
2015-09-21 20:41:30 -0700

    Doc comments for Query

--- src/index/query.rs
+++ src/index/query.rs
@@ -1,6 +1,18 @@
+/// A Query organizes a search of an inverted index.
+/// It is recursively hierarchical, allowing flexibility
+/// in exactly how a search is specified
 #[derive(Debug)]
 pub enum Query<'a> {
+    /// The simplest query, represents a search using
+    /// the given string
     Match(&'a str),
+
+    /// A query requesting the intersection of the documents 
+    /// returned in each sub-query
     And(&'a [Query<'a>]),
+
+
+    /// A query requesting the union of the documents returned
+    /// in each sub-query
     Or(&'a [Query<'a>]),
 }
