26e845d6f9c50cf003c2f91a50303adb2f375379
Tim Kuehn <timothy.j.kuehn@gmail.com>
2015-10-12 19:44:15 -0700

    Derive all possible traits for Query

--- src/query.rs
+++ src/query.rs
@@ -1,7 +1,7 @@
 /// A Query organizes a search of an inverted index.
 /// It is recursively hierarchical, allowing flexibility
 /// in exactly how a search is specified
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, RustcEncodable)]
 pub enum Query<'a> {
     /// The simplest query, represents a search using
     /// the given string
