2a3984950e7e8d5fb1a05b068fab1cba0d2ad0da
Tim Kuehn <timothy.j.kuehn@gmail.com>
2015-10-13 17:11:58 -0700

    Add successor trait for chars

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,6 +2,10 @@
 pub mod btree_map_ext;
 /// Contains the Coalesce trait, for performing coalescence on collections and its items.
 pub mod coalesce;
+/// Contains the Successor trait, which is the same thing as `std::iter::Step`, except it's
+/// implemented for chars.
+pub mod successor;
 
 pub use self::btree_map_ext::{BTreeMapExt, Intersection};
 pub use self::coalesce::{Coalesce, Merge};
+pub use self::successor::Successor;
---
+++ src/util/successor.rs
@@ -0,0 +1,20 @@
+use std::char;
+
+/// A trait for types whose values have well-defined successors.
+pub trait Successor: Sized {
+    /// Returns the successor to self, if any exists.
+    fn successor(&self) -> Option<Self>;
+}
+
+impl Successor for char {
+    #[inline]
+    // Implementation lifted from https://github.com/huonw/char-iter/blob/master/src/lib.rs#L77
+    fn successor(&self) -> Option<char> {
+        const SUR_START: u32 = 0xD800;
+        const SUR_END: u32 = 0xDFFF;
+        const BEFORE_SUR: u32 = SUR_START - 1;
+        const AFTER_SUR: u32 = SUR_END + 1;
+        let val = *self as u32;
+        char::from_u32(if val == BEFORE_SUR {AFTER_SUR} else {val + 1})
+    }
+}
