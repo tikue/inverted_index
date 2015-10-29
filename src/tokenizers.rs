24d15ca66578cc76d128604fcfd15138653c200e
Tim Kuehn <timothy.j.kuehn@gmail.com>
2015-10-29 01:28:24 -0700

    WIP

--- src/tokenizers.rs
+++ src/tokenizers.rs
@@ -117,7 +117,7 @@ impl EnglishUtf8<io::Cursor<Vec<u8>>> {
     }
 }
 
-impl<B: io::BufRead> Tokenizer for EnglishUtf8<B> {
+impl<Buf: io::BufRead> Tokenizer for EnglishUtf8<Buf> {
     fn read(&mut self, tok: &mut Token) -> io::Result<bool> {
         let mut consumed = 0;
         tok.token.clear();
@@ -167,6 +167,110 @@ impl<B: io::BufRead> Tokenizer for EnglishUtf8<B> {
         tok.position.position = self.num_tokens;
         self.num_tokens += 1;
         Ok(true)
+    }
+}
+
+/// An analyzer that tokenizes its input and returns each subslice of each token that starts from
+/// the first char.
+pub struct NgramsAnalyzer<Tknzr: Tokenizer> {
+    tokenizer: Tknzr,
+    next: Vec<Token>,
+}
+
+impl<Tknzr: Tokenizer> Tokenizer for NgramsAnalyzer<Tknzr> {
+    fn read(&mut self, tok: &mut Token) -> io::Result<bool> {
+        match self.next.pop() {
+            Some(next) => { 
+                *tok = next;
+                Ok(true)
+            }
+            None => {
+                match self.tokenizer.read(tok) {
+                    done @ Ok(false) | Err(_) => done,
+                    Ok(true) => {
+                        let chars: Vec<_> = tok.token.char_indices().collect();
+                        (1..chars.len() + 1).map(|to| {
+                            let word = chars[..to]
+                                           .iter()
+                                           .flat_map(|&(_, c)| c)
+                                           .collect();
+                            let start = self.chars[0].0;
+                            let (last_idx, last_char) = self.chars[to - 1];
+                            let finish = last_idx + last_char.len_utf8();
+                            (word, Position::new((start, finish), self.position))
+                    }
+                }
+            }
+        }
+    }
+
+    type TokenPositions = iter::FlatMap<
+                    iter::Enumerate<WordPositionsNoWhitespace<'a>>,
+                    NgramsIter,
+                    NgramsFn>;
+
+    fn analyze(&self, s: &'a str) -> Self::TokenPositions {
+
+        s.char_indices()
+         .group_by(is_whitespace as fn(&(usize, char)) -> bool)
+         .filter(not_whitespace as fn(&(bool, Vec<(usize, char)>)) -> bool)
+         .enumerate()
+         .flat_map(ngrams)
+    }
+}
+
+type WordPositions<'a> = GroupBy<bool, CharIndices<'a>, fn(&(usize, char)) -> bool>;
+type WordPositionsNoWhitespace<'a> = iter::Filter<
+    WordPositions<'a>,
+    fn(&(bool, Vec<(usize, char)>)) -> bool>;
+type NgramsIter = iter::Map<ops::Range<usize>, Ngrams>;
+type NgramsFn = fn((usize, (bool, Vec<(usize, char)>))) -> NgramsIter;
+type OnlyTokens<I> = iter::Map<I, fn((String, Position)) -> String>;
+type WhitespaceTokenPositions<'a> = iter::Map<iter::Enumerate<WordPositionsNoWhitespace<'a>>, 
+                        fn((usize, (bool, Vec<(usize, char)>))) -> (String, Position)>;
+
+fn not_whitespace(&(is_whitespace, _): &(bool, Vec<(usize, char)>)) -> bool {
+    !is_whitespace
+}
+
+fn is_whitespace(&(_, c): &(usize, char)) -> bool {
+    c.is_whitespace()
+}
+
+struct Ngrams {
+    position: usize,
+    chars: Vec<(usize, char)>,
+}
+
+impl Ngrams {
+    fn new(position: usize, chars: Vec<(usize, char)>) -> Ngrams {
+        Ngrams {
+            position: position,
+            chars: chars,
+        }
+    }
+}
+
+impl Fn<(usize,)> for Ngrams {
+    extern "rust-call" fn call(&self, (to,): (usize,)) -> (String, Position) {
+        let word = self.chars[..to].iter().flat_map(|&(_, c)| c.to_lowercase()).collect();
+        let start = self.chars[0].0;
+        let (last_idx, last_char) = self.chars[to - 1];
+        let finish = last_idx + last_char.len_utf8();
+        (word, Position::new((start, finish), self.position))
+    }
+}
+
+impl FnMut<(usize,)> for Ngrams {
+    extern "rust-call" fn call_mut(&mut self, to: (usize,)) -> (String, Position) {
+        self.call(to)
+    }
+}
+
+impl FnOnce<(usize,)> for Ngrams {
+    type Output = (String, Position);
+    extern "rust-call" fn call_once(self, to: (usize,)) -> (String, Position) {
+        self.call(to)
     }
 }
 
